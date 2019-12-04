d173879d25c11042f4de685b331469f05114a03a
Drew Graham <dmg9626@gmail.com>
2019-12-04 17:56:40 -0500

    added auto_commit.sh script + .gitignore to ignore log file

---
+++ auto_commit.sh
@@ -0,0 +1,17 @@
+log_file="auto_commit_log.txt"
+
+# repeat until user presses CTRL + C
+while true; do
+        # sleep for 5 minutes
+        sleep 5m
+
+        # pull before attempting to commit anything (in case running on multiple computers)
+        git pull >> $log_file
+        # add all to index
+        git add . >> $log_file
+
+        # commit with timestamp log
+        commit_log=`date`
+        git commit -m "$commit_log" >> $log_file
+        git push >> $log_file
+done
