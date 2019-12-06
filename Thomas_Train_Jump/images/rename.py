d9ac688d4304a85f9d262a7b7ff6dce6d3d13967
Drew Graham <Drew Graham>
2019-12-06 15:36:27 -0500

    updated rename.py with proper zero-padding in filenames - now works with frames below 100

--- Thomas_Train_Jump/images/rename.py
+++ Thomas_Train_Jump/images/rename.py
@@ -1,8 +1,17 @@
 import os
 import sys
 
-# move to directory
+# exit if not enough args
+if len(sys.argv) < 3:
+    print("usage: rename.py {directory} {start_at}")
+    print("{directory}: directory containing the frames")
+    print("{start_at}: frame number to start at")
+    sys.exit(1)
+
+# get directory and start count
 directory = sys.argv[1]
+
+# move to directory
 os.chdir(directory)
 print("working directory: {}".format(directory))
 
@@ -13,14 +22,21 @@ files = os.listdir()
 # filter out other files, sort by increasing
 files = [f for f in files if prefix in f]
 files.sort()
-print(files)
 print("Renaming {} files...".format(len(files)))
 
 # rename each file recursively
 start_count = (int)(sys.argv[2])
-inc = 1
+inc = 0
 for f in files:
-    filename = "{}_0{}.png".format(prefix, start_count+inc)
-    print("renaming {} to {}".format(f, filename))
-    inc += 1
+    # get frame number (padded with 4 zeroes)
+    frame_num = str(start_count+inc).zfill(4)
+    
+    # generate filename
+    filename = "{}_{}.png".format(prefix, frame_num)
+    print("{} -> {}".format(f, filename))
+
+    # rename file
     os.rename(f, filename)
+
+    # increment & repeat
+    inc += 1
